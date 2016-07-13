26ef3e8abf9ba1e28e1d8e50dc615e40fe564131
David Smitmanis <david.smitmanis@aveso.se>
2016-07-13 15:08:50 +0200

    Add streets

--- person/address.go
+++ person/address.go
@@ -2,12 +2,14 @@ package person
 
 import (
 	"github.com/masenius/personapi/data"
+	"strconv"
 )
 
 type PostalAddress struct {
-	StreetAddress string `json:"streetAddress"`
-	Code          string `json:"code"`
-	Locality      string `json:"locality"`
+	Street       string `json:"streetAddress"`
+	StreetNumber string `json:"streetNumber"`
+	Code         string `json:"code"`
+	Locality     string `json:"locality"`
 }
 
 type postalCode struct {
@@ -15,12 +17,40 @@ type postalCode struct {
 	Locality string `json:"locality"`
 }
 
-func RandomPostalCode() postalCode {
+type streetAddress struct {
+	Street string `json:"street"`
+	Number string `json:"number"`
+}
+
+func randomPostalCode() postalCode {
 	pc := data.PostalCodes[randgen.Intn(len(data.PostalCodes))]
 	return postalCode{pc.Code, pc.Locality}
 }
 
+func randomStreetAddress() streetAddress {
+	return streetAddress{Street: randomStreetName(), Number: randomStreetNumber()}
+}
+
+func randomStreetName() string {
+	return data.StreetNames[randgen.Intn(len(data.StreetNames))]
+}
+
+func randomStreetNumber() string {
+	number := randgen.Intn(75)
+	var letter string
+	// Add a letter in 1/25 cases
+	useLetterRoll := randgen.Intn(25)
+	if useLetterRoll == 0 {
+		// Use A-G as address letters.
+		// Get the codepoint for one of these
+		letterCode := randgen.Intn(7) + 65
+		letter = string(letterCode)
+	}
+	return strconv.Itoa(number) + letter
+}
+
 func RandomAddress() PostalAddress {
-	pc := RandomPostalCode()
-	return PostalAddress{StreetAddress: "Statiska gatan 1", Code: pc.Code, Locality: pc.Locality}
+	pc := randomPostalCode()
+	sa := randomStreetAddress()
+	return PostalAddress{Street: sa.Street, StreetNumber: sa.Number, Code: pc.Code, Locality: pc.Locality}
 }
